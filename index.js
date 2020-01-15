60975f34434e7809ccc3cbd6bb992169cf325c61
toneiojimz <ajimeneza77@gmail.com>
2020-01-15 15:16:05 -0800

    works

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server.js');
 
-server.listen(4050, () => {
-  console.log('\n* Server Running on http://localhost:4050 *\n');
+server.listen(3333, () => {
+  console.log('\n* Server Running on http://localhost:3333 *\n');
 });
 
